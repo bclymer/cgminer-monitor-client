f819a6294dcb50cc05abaf5575b2c648209c07e1
Brian Clymer <crdnilfan@gmail.com>
2013-12-31 19:54:04 -0600

    Initial Commit

---
+++ client.go
@@ -0,0 +1,251 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"mime/multipart"
+	"net"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type MinerCommand struct {
+	Command   string `json:"command"`
+	Parameter string `json:"parameter"`
+}
+
+type Config struct {
+	Interval       int           `json:"interval"`
+	ParsedInterval time.Duration `json:"-"`
+	ServerHost     string        `json:"serverHost"`
+	ServerPort     string        `json:"serverPort"`
+	MinerHost      string        `json:"minerHost"`
+	MinerPort      string        `json:"minerPort"`
+}
+
+type DeviceStats struct {
+	Status []struct {
+		Description string `json:"Description"`
+		Status      string `json:"STATUS"`
+		When        uint64 `json:"When"`
+	} `json:"STATUS"`
+	Devs []struct {
+		GPU               int     `json:"GPU"`
+		Enabled           string  `json:"Enabled"`
+		Status            string  `json:"Status"`
+		Temperature       float64 `json:"Temperature"`
+		FanSpeed          int     `json:"Fan Speed"`
+		FanPercent        int     `json:"Fan Percent"`
+		GpuClock          int     `json:"GPU Clock"`
+		MemClock          int     `json:"Memory Clock"`
+		GpuVoltage        float64 `json:"GPU Voltage"`
+		GpuActivity       int     `json:"GPU Activity"`
+		Powertune         int     `json:"Powertune"`
+		MhsAv             float64 `json:"MHS av"`
+		MhsFiveSeconds    float64 `json:"MHS 5s"`
+		Accepted          int     `json:"Accepted"`
+		Rejected          int     `json:"Rejected"`
+		HardwareErrors    int     `json:"Hardware Errors"`
+		Utility           float64 `json:"Utility"`
+		Intensity         string  `json:"Intensity"`
+		LastSharePool     uint64  `json:"Last Share Pool"`
+		LastShareTime     uint64  `json:"Last Share Time"`
+		TotalMh           float64 `json:"Total MH"`
+		DiffOneWork       uint64  `json:"Diff1 Work"`
+		DiffAccepted      float64 `json:"Difficulty Accepted"`
+		DiffRejected      float64 `json:"Difficulty Rejected"`
+		LastShareDiff     float64 `json:"Last Share Difficulty"`
+		LastValidWorkd    uint64  `json:"Last Valid Work"`
+		DeviceHardwarePct float64 `json:"Device Hardware%"`
+		DeviceRejectedPct float64 `json:"Device Rejected%"`
+		DeviceElapsed     uint64  `json:"Device Elapsed"`
+	} `json:"DEVS"`
+}
+
+var (
+	uploadQueue = make(chan (os.FileInfo))
+	config      Config
+)
+
+func main() {
+	loadConfig()
+	os.Mkdir("./stats", 7777)
+	go monitorFsForStats()
+	go uploadStat()
+	for {
+		time.Sleep(config.ParsedInterval * time.Second)
+
+		response, err := queryMiner("devs", "")
+		if err != nil {
+			continue
+		}
+		var devs DeviceStats
+		err = json.Unmarshal([]byte(response), &devs)
+		if err != nil {
+			fmt.Println("Parse Error:", err)
+			continue
+		} else {
+			//fmt.Println("Response:", strings.TrimRight(string(response), "\x00"))
+		}
+		go writeDeviceStats(devs)
+	}
+}
+
+func queryMiner(command, param string) (string, error) {
+	commandDto := MinerCommand{
+		Command:   command,
+		Parameter: param,
+	}
+	commandBytes, err := json.Marshal(commandDto)
+	if err != nil {
+		fmt.Println("Marshal Error:", err)
+		return "", err
+	}
+	conn, err := net.Dial("tcp", config.MinerHost+":"+config.MinerPort)
+	if err != nil {
+		fmt.Println("Dail Error:", err)
+		return "", err
+	}
+	conn.SetReadDeadline(time.Now().Add(10 * time.Second))
+	_, err = conn.Write(commandBytes)
+	if err != nil {
+		fmt.Println("Write Error:", err)
+		return "", err
+	}
+	response := make([]byte, 4096, 4096)
+	conn.Read(response)
+	if err != nil {
+		fmt.Println("Read Error:", err)
+		return "", err
+	}
+	return strings.TrimRight(string(response), "\x00"), nil
+}
+
+func loadConfig() {
+	content, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal(content, &config)
+	if err != nil {
+		panic(err)
+	}
+	config.ParsedInterval = time.Duration(config.Interval)
+	fmt.Println("Server is: " + config.ServerHost + ":" + config.ServerPort)
+	fmt.Println("Miner is: " + config.MinerHost + ":" + config.MinerPort)
+	fmt.Println("Querying every", config.Interval, "seconds")
+}
+
+func writeDeviceStats(devStats DeviceStats) {
+	stats, err := json.Marshal(devStats)
+	if err != nil {
+		fmt.Println("Failed marshaling devStats", err)
+		return
+	}
+	err = ioutil.WriteFile("stats/"+strconv.FormatUint(devStats.Status[0].When, 10), stats, 0644)
+	if err != nil {
+		fmt.Println("Failed writing the file", err)
+		return
+	}
+}
+
+func monitorFsForStats() {
+	for {
+		time.Sleep(config.ParsedInterval * time.Second)
+		fmt.Println("Scanning for files to upload")
+		files, err := ioutil.ReadDir("./stats/")
+		if err != nil {
+			fmt.Println("Failed reading ./stats/ dir", err)
+			continue
+		}
+		fmt.Println("Found", len(files), "files to upload")
+		for _, file := range files {
+			uploadQueue <- file
+		}
+	}
+}
+
+func uploadStat() {
+	for {
+		select {
+		case file := <-uploadQueue:
+			fmt.Println("Uploading", file.Name())
+			if err := postStatFile(file); err != nil {
+				fmt.Println("Failed to upload file", err)
+			} else {
+				err := os.Remove("./stats/" + file.Name())
+				if err != nil {
+					fmt.Println("Failed deleting", file.Name(), err)
+				} else {
+					fmt.Println("Deleted", file.Name())
+				}
+			}
+			break
+		}
+	}
+}
+
+func postStatFile(file os.FileInfo) error {
+	extraParams := map[string]string{
+		"name": file.Name(),
+	}
+	request, err := newfileUploadRequest("http://"+config.ServerHost+":"+config.ServerPort+"/stats", extraParams, "file", "./stats/"+file.Name())
+	if err != nil {
+		return err
+	}
+	client := &http.Client{}
+	resp, err := client.Do(request)
+	if err != nil {
+		return err
+	} else {
+		body := &bytes.Buffer{}
+		_, err := body.ReadFrom(resp.Body)
+		if err != nil {
+			return err
+		}
+		resp.Body.Close()
+		fmt.Println(resp.StatusCode)
+		fmt.Println(resp.Header)
+		fmt.Println(body)
+	}
+	return nil
+}
+
+// Creates a new file upload http request with optional extra params
+func newfileUploadRequest(uri string, params map[string]string, paramName, path string) (*http.Request, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	part, err := writer.CreateFormFile(paramName, filepath.Base(path))
+	if err != nil {
+		return nil, err
+	}
+	_, err = io.Copy(part, file)
+
+	for key, val := range params {
+		_ = writer.WriteField(key, val)
+	}
+	err = writer.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", uri, body)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	return req, nil
+}
